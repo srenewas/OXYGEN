cbdee23f9cde82a470dab6f1eb61c8d015a804da
SEENU IGLESIAS <SEENU IGLESIAS@DESKTOP-S8ND7H3>
2019-10-25 11:49:23 +0530

    commit

---
+++ test/src/com/seenu/test.java
@@ -0,0 +1,11 @@
+package com.seenu;
+
+public class test {
+	public static void main(String[] args) {
+		String s = new String("seanu");
+		char[] ch = s.toCharArray();
+		for(int i=ch.length;i>0;i--) {
+			System.out.println(ch);
+		}
+	}
+}
